6e50af5494741198d9b8eb0f1a75fe563acf64e1
Marc Troesken <m.troesken@mail.de>
2019-05-16 17:35:24 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="font-sans text-grey-dark leading-normal flex flex-col min-h-screen">
+        <header class="border-t-14 border-red">
+            <nav class="container mx-auto flex justify-between items-center py-8">
+                <a href="/">
+                    <img class="w-64" src="@/assets/images/Troesken-Logo.png" alt="Logo Marc Trösken">
+                </a>
+                <ul class="uppercase tracking-wide font-bold flex items-center list-reset">
+                    <li class="mr-8">
+                        <a href="#" class="text-grey-darkest hover:text-grey-dark no-underline">Github</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <div class="flex-grow">
+            <nuxt />
+        </div>
+
+        <footer class="bg-red text-white">
+            <div class="container mx-auto flex items-center justify-between py-8">
+                <div>Copyright 2019. All rights reserved</div>
+                <ul class="list-reset flex items-center">
+                    <li class="mr-8">
+                        <a class="text-white" href="https://www.facebook.com/marc.troesken"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="mr-8">
+                        <a class="text-white" href="https://twitter.com/MarcTroesken"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="mr-8">
+                        <a class="text-white" href="https://www.twitch.tv/mulibu2k"><i class="fa fa-twitch" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="mr-8">
+                        <a class="text-white" href="https://www.xing.com/profile/Marc_Troesken"><i class="fa fa-xing" aria-hidden="true"></i></a>
+                    </li>
+                    <li class="mr-8">
+                        <a class="text-white" href="https://github.com/MarcTroesken"><i class="fa fa-github" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <section class="hero container-inner mx-auto flex justify-between py-16 text-grey-darkest">
+        <div class="text-4xl font-bold w-4/5">
+            <div>Full-stack web developer</div>
+            <div class="text-red">Welcome to my website</div>
+        </div>
+    </section>
+</template>
